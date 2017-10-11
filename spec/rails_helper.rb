5ab55342c59137d2661ccafba643e231d0fe57c1
mokuo <tennis10988.yk@gmail.com>
2017-10-11 22:12:19 +0900

    spec を通し、database_rewinder の警告をなくす

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,8 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # https://github.com/amatsuda/database_rewinder#mysql--use_transactional_tests-specific-problems
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
