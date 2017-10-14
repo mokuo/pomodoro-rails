6855d187376d7c67744747ffd6cadc54fe154d30
木田悠一郎 <yuichiro@kidayuuichirounoMacBook-Pro.local>
2017-10-14 11:04:30 +0900

    jQuery を参照できるようにする
    - https://github.com/rails/webpacker/blob/master/docs/webpack.md#plugins

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.set(
+  'Provide',
+  new webpack.ProvidePlugin({
+    jQuery: 'jquery'
+  })
+)
 
 module.exports = environment
