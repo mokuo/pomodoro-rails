304e6a6c861280743d86ae58dfe77d338815fd2a
木田悠一郎 <yuichiro@kidayuuichirounoMacBook-Pro.local>
2017-10-21 22:08:48 +0900

    ログアウト機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  include UserAuthenticatable
+
+  before_action :restrict_access
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'projects#index'
-  resources :sessions, only: [:new, :create]
+  resource :sessions, only: [:new, :create, :destroy]
   resources :projects, except: :show do
     resource :stop, only: [:update, :destroy], module: :projects
   end
