826037eef08be1cea4eda40adac583c23adfb166
mokuo <tennis10988.yk@gmail.com>
2017-10-21 22:08:48 +0900

    ログアウト機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,22 @@
 class SessionsController < ApplicationController
+  skip_before_action :restrict_access, except: :destroy
+
   def new
   end
 
   def create
-    if User.find_by(name: params[:name]).tapp.try(:authenticate, params[:password]).tapp
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+      login(user)
       redirect_to root_path, notice: 'ログインしました'
     else
       flash[:alert] = 'ユーザー名とパスワードが一致しません'
       render :new
     end
+  end
+
+  def destroy
+    logout
+    redirect_to new_sessions_path, notice: 'ログアウトしました'
   end
 end
