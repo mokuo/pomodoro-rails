819881b17fa359e35bffdaa1579c5382a58ac6d9
mokuo <tennis10988.yk@gmail.com>
2017-11-05 14:30:56 +0900

    Task に todo_on カラムを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -9,10 +9,12 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  project_id :integer
+#  todo_on    :date
 #
 # Indexes
 #
 #  index_tasks_on_project_id  (project_id)
+#  index_tasks_on_todo_on     (todo_on)
 #  index_tasks_on_user_id     (user_id)
 #
 # Foreign Keys
