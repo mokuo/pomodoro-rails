ba1efd43bad5d76f9ed57786f1a7d2f9ecb4c9af
mokuo <tennis10988.yk@gmail.com>
2017-11-09 16:41:12 +0900

    Task の User への参照を削除する

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,6 @@
 # Table name: tasks
 #
 #  id         :integer          not null, primary key
-#  user_id    :integer          not null
 #  name       :string(255)      not null
 #  done       :boolean          default(FALSE), not null
 #  created_at :datetime         not null
@@ -15,12 +14,10 @@
 #
 #  index_tasks_on_project_id  (project_id)
 #  index_tasks_on_todo_on     (todo_on)
-#  index_tasks_on_user_id     (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (project_id => projects.id)
-#  fk_rails_...  (user_id => users.id)
 #
 
 class Task < ApplicationRecord
