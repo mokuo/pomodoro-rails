cadb5377d4f85aa3a2f8c722e343d388e97a1bb8
mokuo <tennis10988.yk@gmail.com>
2017-11-17 22:32:55 +0900

    日付パラメータを必須にし、エラーハンドリングも追加

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -2,6 +2,12 @@ class Api::TodosController < Api::BaseController
   def index
     # TODO: フロントエンドの実装に入り次第、外す
     current_user = User.take
-    @projects = current_user.projects.eager_load(tasks: :pomodoros).where(tasks: { todo_on: params[:date] })
+    @projects = current_user.projects.eager_load(tasks: :pomodoros).where(tasks: { todo_on: date_params })
+  end
+
+  private
+
+  def date_params
+    params.require(:date)
   end
 end
