ce2f3a5d7fc26d8f791708fbc2ba40c87b9c28f0
mokuo <tennis10988.yk@gmail.com>
2017-11-19 17:51:55 +0900

    バリデーションエラーの場合のエラー処理を追加

--- app/controllers/concerns/api_error_handle_actions.rb
+++ app/controllers/concerns/api_error_handle_actions.rb
@@ -1,17 +1,19 @@
 concern :ApiErrorHandleActions do
   included do
     rescue_from ActionController::ParameterMissing do
-      render_json(400, '実行に必要なパラメーターが送信されていません')
+      render_json(400, ['実行に必要なパラメーターが送信されていません'])
     end
 
     rescue_from ActiveRecord::RecordNotFound do
-      render_json(401, '指定されたリソースが見つかりません')
+      render_json(401, ['指定されたリソースが見つかりません'])
     end
+
+    # NOTE: 402はバリデーションエラーで使用
   end
 
   private
 
-  def render_json(code, message)
-    render json: { error: { code: code, message: message } }
+  def render_json(code, messages)
+    render json: { error: { code: code, messages: messages } }
   end
 end
