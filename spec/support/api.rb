05ef7495bf484855f690928fd5e50c66a173962d
mokuo <tennis10988.yk@gmail.com>
2017-11-19 17:11:38 +0900

    todos_spec のパラメータ不足の場合のテストを追加

--- spec/support/api.rb
+++
@@ -1,9 +0,0 @@
-shared_examples '処理成功' do
-  it 'ステータスコード 200 を返す' do
-    expect(response).to have_http_status 200
-  end
-
-  it 'エラーコード 0 を返す' do
-    expect(response.body).to be_json_eql(0).at_path('error/code')
-  end
-end
