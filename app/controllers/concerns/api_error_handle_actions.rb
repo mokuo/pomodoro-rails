7bf2b8510607321137591edaacfe45fa8e2bebb5
mokuo <tennis10988.yk@gmail.com>
2017-11-19 17:25:35 +0900

    存在しないプロジェクトIDを指定された場合のエラー処理を追加

--- app/controllers/concerns/api_error_handle_actions.rb
+++ app/controllers/concerns/api_error_handle_actions.rb
@@ -1,7 +1,17 @@
 concern :ApiErrorHandleActions do
   included do
     rescue_from ActionController::ParameterMissing do
-      render json: { error: { code: 400, message: '実行に必要なパラメーターが送信されていません' } }
+      render_json(400, '実行に必要なパラメーターが送信されていません')
     end
+
+    rescue_from ActiveRecord::RecordNotFound do
+      render_json(401, '指定されたリソースが見つかりません')
+    end
+  end
+
+  private
+
+  def render_json(code, message)
+    render json: { error: { code: code, message: message } }
   end
 end
