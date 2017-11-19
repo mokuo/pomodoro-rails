f5aaae1216e3b938dabf20db41ddd693256ed492
mokuo <tennis10988.yk@gmail.com>
2017-11-19 18:42:44 +0900

    異常系のテストを実装

--- spec/support/api_errors.rb
+++ spec/support/api_errors.rb
@@ -18,6 +18,16 @@ shared_examples 'パラメーター不足' do
   end
 
   it 'エラーメッセージを返す' do
-    expect(response.body).to be_json_eql('実行に必要なパラメーターが送信されていません'.to_json).at_path('error/message')
+    expect(response.body).to be_json_eql('実行に必要なパラメーターが送信されていません'.to_json).at_path('error/messages/0')
+  end
+end
+
+shared_examples 'バリデーションエラー' do
+  it 'ステータスコード 200 を返す' do
+    expect(response).to have_http_status 200
+  end
+
+  it 'エラーコード 402 を返す' do
+    expect(response.body).to be_json_eql(402).at_path('error/code')
   end
 end
