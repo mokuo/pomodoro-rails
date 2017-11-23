9d35ac2330d97e288583a0ec653aaca2de5ddf53
mokuo <tennis10988.yk@gmail.com>
2017-11-23 21:34:59 +0900

    異常系のテストを追加

--- spec/requests/api/pomodoros_spec.rb
+++ spec/requests/api/pomodoros_spec.rb
@@ -41,5 +41,44 @@ RSpec.describe 'Api::Pomodoros', type: :request do
         expect(response.body).to be_json_eql(task_id).at_path('pomodoro/task_id')
       end
     end
+
+    context '異常系' do
+      context 'パラメーターを指定しない時' do
+        let(:task_id) { task.id }
+        let(:params) { '' }
+
+        it_behaves_like 'パラメーター不足'
+
+        it 'ポモドーロが作成されない', :skip_before do
+          expect { subject }.not_to change { Pomodoro.count }
+        end
+      end
+
+      context '存在しないタスクIDを指定した時' do
+        let(:task_id) { 0 }
+        let(:params) { '{ "box": "square" }' }
+
+        it_behaves_like '存在しないリソース'
+
+        it 'ポモドーロが作成されない', :skip_before do
+          expect { subject }.not_to change { Pomodoro.count }
+        end
+      end
+
+      context 'box に空文字を指定した時' do
+        let(:task_id) { task.id }
+        let(:params) { '{ "box": "" }' }
+
+        it_behaves_like 'バリデーションエラー'
+
+        it 'ポモドーロが作成されない', :skip_before do
+          expect { subject }.not_to change { Pomodoro.count }
+        end
+
+        it 'バリデーションエラーメッセージを返す' do
+          expect(response.body).to be_json_eql('ボックスを入力してください'.to_json).at_path('error/messages/0')
+        end
+      end
+    end
   end
 end
