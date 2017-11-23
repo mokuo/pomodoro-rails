d2002d789332d1968fcebb13bc7a410eab4ed7e6
mokuo <tennis10988.yk@gmail.com>
2017-11-23 20:14:55 +0900

    正常系のテストを通す

---
+++ spec/requests/api/pomodoros_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::Pomodoros', type: :request do
+  include_context 'ログインしている'
+
+  describe 'POST /api/v1/tasks/:task_id/pomodoros' do
+    subject { post "/api/v1/tasks/#{task_id}/pomodoros", params: params, headers: headers }
+    let!(:task) { create :task }
+    let(:headers) { {
+      'ACCEPT': 'application/json',
+      'CONTENT_TYPE': 'application/json'
+    } }
+
+    before do |example|
+      subject unless example.metadata[:skip_before]
+    end
+
+    context '正常系' do
+      let(:task_id) { task.id }
+      let(:params) { '{ "box": "square" }' }
+
+      it_behaves_like '処理成功'
+
+      it 'ポモドーロを新規作成する', :skip_before do
+        expect { subject }.to change { Pomodoro.count }.by(1)
+      end
+
+      it 'ポモドーロIDを返す' do
+        expect(response.body).to be_json_eql(Pomodoro.last.id).at_path('pomodoro/id')
+      end
+
+      it 'box を返す' do
+        expect(response.body).to be_json_eql('square'.to_json).at_path('pomodoro/box')
+      end
+
+      it 'done を返す' do
+        expect(response.body).to be_json_eql(false).at_path('pomodoro/done')
+      end
+
+      it 'タスクIDを返す' do
+        expect(response.body).to be_json_eql(task_id).at_path('pomodoro/task_id')
+      end
+    end
+  end
+end
