dcb92142210927b34a7bd82c1d43085c9e279800
mokuo <tennis10988.yk@gmail.com>
2017-11-25 00:05:51 +0900

    current_user をセッションから取るように修正

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,7 +1,5 @@
 class Api::ProjectsController < Api::BaseController
   def index
-    # TODO: フロントエンドの実装に入り次第、外す
-    current_user = User.take
     @projects = current_user.projects.without_default
   end
 end
--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,7 +1,5 @@
 class Api::TodosController < Api::BaseController
   def index
-    # TODO: フロントエンドの実装に入り次第、外す
-    current_user = User.take
     @projects = current_user.projects.eager_load(tasks: :pomodoros).where(tasks: { todo_on: date_params })
   end
 
