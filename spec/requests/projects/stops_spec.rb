92121a139f485532214a4ffc41235ebd147a49e1
mokuo <tennis10988.yk@gmail.com>
2017-12-03 21:33:51 +0900

    他のユーザーのプロジェクトを停止・再開できない

--- spec/requests/projects/stops_spec.rb
+++ spec/requests/projects/stops_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Projects::Stops', type: :request do
   before do |example|
-    login unless example.metadata[:skip_login]
+    login(user) unless example.metadata[:skip_login]
   end
+  let(:user) { create :user }
 
   describe 'PATCH /projects/:project_id/stop' do
     subject { patch "/projects/#{project.id}/stop" }
-    let!(:project) { create :project }
 
-    before { subject }
+    before do |example|
+      subject unless example.metadata[:skip_subject]
+    end
 
     context '正常系' do
+      let!(:project) { create :project, user: user }
+
       it 'プロジェクトを停止する' do
         expect { project.reload }.to change { project.stopped_at? }.from(false).to(true)
       end
@@ -22,17 +26,26 @@ RSpec.describe 'Projects::Stops', type: :request do
     end
 
     context '異常系' do
-      include_context 'ログインしていない時'
+      include_context 'ログインしていない時' do
+        let!(:project) { create :project, user: user }
+      end
+
+      include_context '他のユーザーのリソースを指定した時' do
+        let!(:project) { create :project }
+      end
     end
   end
 
   describe 'DELETE /projects/:project_id/stop' do
     subject { delete "/projects/#{project.id}/stop" }
-    let!(:project) { create :project, stopped_at: DateTime.current }
 
-    before { subject }
+    before do |example|
+      subject unless example.metadata[:skip_subject]
+    end
 
     context '正常系' do
+      let!(:project) { create :project, stopped_at: DateTime.current, user: user }
+
       it 'プロジェクトを再開する' do
         expect { project.reload }.to change { project.stopped_at? }.from(true).to(false)
       end
@@ -43,7 +56,13 @@ RSpec.describe 'Projects::Stops', type: :request do
     end
 
     context '異常系' do
-      include_context 'ログインしていない時'
+      include_context 'ログインしていない時' do
+        let!(:project) { create :project, stopped_at: DateTime.current, user: user }
+      end
+
+      include_context '他のユーザーのリソースを指定した時' do
+        let!(:project) { create :project, stopped_at: DateTime.current }
+      end
     end
   end
 end
