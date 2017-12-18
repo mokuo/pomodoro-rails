4dc2d6189af4a6a179679dcc5cbf16f1e19368e1
mokuo <tennis10988.yk@gmail.com>
2017-12-18 22:28:02 +0900

    ポモドーロを表示

---
+++ app/javascript/packs/components/Pomodoro.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const symbol = box => {
+  switch (box) {
+    case 'square':
+      return '□'
+    case 'circle':
+      return '○'
+    case 'triangle':
+      return '△'
+    default:
+      return ''
+  }
+}
+
+const Pomodoro = props =>
+  (
+    <td className="pomodoro-box" width="48">{symbol(props.box)}</td>
+  )
+
+Pomodoro.propTypes = {
+  box: PropTypes.string
+}
+
+Pomodoro.defaultProps = {
+  box: ''
+}
+
+export default Pomodoro
--- app/javascript/packs/components/Task.js
+++ app/javascript/packs/components/Task.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Pomodoro from './Pomodoro'
 
-const Task = props =>
-  (
+const Task = props => {
+  const emptyPomodoros = []
+
+  for (let i = 0; i < 8 - props.pomodoros.length; i += 1) {
+    emptyPomodoros.push(<Pomodoro key={i} />)
+  }
+
+  return (
     <tr>
       <td className="pl-4" width="10"><input type="checkbox" /></td>
       <td>{props.name}</td>
+      {props.pomodoros.map(pomodoro => (
+        <Pomodoro key={pomodoro.id} {...pomodoro} />
+      ))}
+      {emptyPomodoros}
     </tr>
   )
+}
 
 Task.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  pomodoros: PropTypes.arrayOf(PropTypes.object).isRequired
 }
 
 export default Task
