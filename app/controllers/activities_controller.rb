6842330c7e4fcc1bb4de126d6706a2d8b36e95da
mokuo <tennis10988.yk@gmail.com>
2018-02-03 22:03:48 +0900

    アクティビティをビューに埋め込む

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
+    @projects = current_user.projects.in_progress.order(id: :desc)
   end
 end
--- spec/requests/activities_spec.rb
+++ spec/requests/activities_spec.rb
@@ -1,16 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Activities', type: :request do
+  let(:user) { create :user }
+
   before do |example|
-    login unless example.metadata[:skip_login]
+    login(user) unless example.metadata[:skip_login]
   end
 
   describe 'GET /todos' do
     subject { get '/todos' }
+    let!(:project) { create :project, user: user }
+    let!(:stopped_project) { create :project, user: user, stopped_at: Date.current }
+    let(:default_projct) { user.projects.find_by(name: Constants::DEFAULT_PROJECT_NAME) }
+    let(:other_user) { create :user }
+    let!(:other_project) { create :project, user: other_user }
 
     before { subject }
 
     context '正常系' do
+      it 'ログインユーザーかつ進行中のプロジェクトを ID の降順で割り当てる' do
+        expect(assigns(:projects)).to eq [project, default_projct]
+      end
+
       it 'アクティビティ在庫シートを表示する' do
         expect(response).to render_template :index
       end
