a390715054073742fd64673c19c72c606adb7886
mokuo <tennis10988.yk@gmail.com>
2018-02-28 22:23:39 +0900

    ポモドーロの順序のバリデーションのテスト

--- spec/models/pomodoro_spec.rb
+++ spec/models/pomodoro_spec.rb
@@ -26,4 +26,139 @@ RSpec.describe Pomodoro, type: :model do
   it 'has a valid factory' do
     expect(pomodoro).to be_valid
   end
+
+  describe 'validate :verify_box_type' do
+    let(:task) { create :task }
+
+    context '正常系' do
+      shared_examples 'バリデーションエラーにならない' do
+        it { expect(pomodoro).to be_valid }
+      end
+
+      context '最初のポモドーロの時' do
+        context 'ボックスが四角の時' do
+          let(:pomodoro) { build :pomodoro, box: 'square', task: task }
+
+          it_behaves_like 'バリデーションエラーにならない'
+        end
+      end
+
+      context '前のポモドーロのボックスが四角の時' do
+        before { create :pomodoro, box: 'square', task: task }
+
+        context 'ボックスが四角の時' do
+          let(:pomodoro) { build :pomodoro, box: 'square', task: task }
+
+          it_behaves_like 'バリデーションエラーにならない'
+        end
+
+        context 'ボックスが丸の時' do
+          let(:pomodoro) { build :pomodoro, box: 'circle', task: task }
+
+          it_behaves_like 'バリデーションエラーにならない'
+        end
+      end
+
+      context '前のポモドーロのボックスが丸の時' do
+        before do
+          create :pomodoro, box: 'square', task: task
+          create :pomodoro, box: 'circle', task: task
+        end
+
+        context 'ボックスが丸の時' do
+          let(:pomodoro) { build :pomodoro, box: 'circle', task: task }
+
+          it_behaves_like 'バリデーションエラーにならない'
+        end
+
+        context 'ボックスが三角の時' do
+          let(:pomodoro) { build :pomodoro, box: 'triangle', task: task }
+
+          it_behaves_like 'バリデーションエラーにならない'
+        end
+      end
+
+      context '前のポモドーロのボックスが丸の時' do
+        context 'ボックスが三角の時' do
+          let(:pomodoro) { build :pomodoro, box: 'triangle', task: task }
+
+          before do
+            create :pomodoro, box: 'square', task: task
+            create :pomodoro, box: 'circle', task: task
+            create :pomodoro, box: 'triangle', task: task
+          end
+
+          it_behaves_like 'バリデーションエラーにならない'
+        end
+      end
+    end
+
+    context '異常系' do
+      shared_examples 'バリデーションエラーになる' do
+        it { expect(pomodoro).to be_invalid }
+
+        it 'バリデーションエラーメッセージが正しい' do
+          pomodoro.valid?
+          expect(pomodoro.errors.full_messages.first).to eq 'ボックスは順序通りに設定してください'
+        end
+      end
+
+      context '最初のポモドーロの時' do
+        context 'ボックスが丸の時' do
+          let(:pomodoro) { build :pomodoro, box: 'circle', task: task }
+
+          it_behaves_like 'バリデーションエラーになる'
+        end
+
+        context 'ボックスが三角の時' do
+          let(:pomodoro) { build :pomodoro, box: 'triangle', task: task }
+
+          it_behaves_like 'バリデーションエラーになる'
+        end
+      end
+
+      context '前のポモドーロのボックスが四角の時' do
+        before { create :pomodoro, box: 'square', task: task }
+
+        context 'ボックスが三角の時' do
+          let(:pomodoro) { build :pomodoro, box: 'triangle', task: task }
+
+          it_behaves_like 'バリデーションエラーになる'
+        end
+      end
+
+      context '前のポモドーロのボックスが丸の時' do
+        before do
+          create :pomodoro, box: 'square', task: task
+          create :pomodoro, box: 'circle', task: task
+        end
+
+        context 'ボックスが四角の時' do
+          let(:pomodoro) { build :pomodoro, box: 'square', task: task }
+
+          it_behaves_like 'バリデーションエラーになる'
+        end
+      end
+
+      context '前のポモドーロのボックスが三角の時' do
+        before do
+          create :pomodoro, box: 'square', task: task
+          create :pomodoro, box: 'circle', task: task
+          create :pomodoro, box: 'triangle', task: task
+        end
+
+        context 'ボックスが四角の時' do
+          let(:pomodoro) { build :pomodoro, box: 'square', task: task }
+
+          it_behaves_like 'バリデーションエラーになる'
+        end
+
+        context 'ボックスが丸の時' do
+          let(:pomodoro) { build :pomodoro, box: 'circle', task: task }
+
+          it_behaves_like 'バリデーションエラーになる'
+        end
+      end
+    end
+  end
 end
